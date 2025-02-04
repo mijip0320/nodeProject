abeee59dc63bd7772e1366a0cc807fff736f10c1
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-02-05 07:53:42 +0900

    feat : 코드 수정

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/friends') {
-    //       res.writeHead(200, {
-    //     'Content-Type': 'application/json',
-        
-        // });
+const server = http.createServer();
+
+const friends = [
+    {
+    id: 0,
+    name : 'Nikola Tesla'
+    },
+     {
+    id: 1,
+    name : 'Sir Isaac Newton'
+    },
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+
+    if (items[1]=== 'friends') {
+
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        id: 1,
-        name : 'Sir Isaac Newton'
-    }))
-    } else if (req.url === '/messages') {
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]))
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+        
+    } else if (items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
